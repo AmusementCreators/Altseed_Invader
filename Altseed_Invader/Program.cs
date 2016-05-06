6cb9b1b790f68912ef71a7248d9b32774975f2b6
wisteria <wisteria@coins.tsukuba.ac.jp>
2016-05-06 12:47:06 +0900

    初期状態

--- Altseed_Invader/Program.cs
+++ Altseed_Invader/Program.cs
@@ -12,18 +12,13 @@ namespace Game
 		[System.STAThread]
 		static void Main(string[] args)
 		{
-			// Altseedを初期化する。
 			asd.Engine.Initialize("Invader", 640, 480, new asd.EngineOption());
-			
-			// Altseedのウインドウが閉じられていないか確認する。
+
 			while (asd.Engine.DoEvents())
 			{
-				// Altseedを更新する。
 				asd.Engine.Update();
 			}
-
-			// Altseedの終了処理をする。
-			asd.Engine.Terminate();
+		asd.Engine.Terminate();
 		}
 	}
 }
