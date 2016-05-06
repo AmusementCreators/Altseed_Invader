48ad655ec798d533de7ce14d93299ed1d2083c4a
wisteria <wisteria@coins.tsukuba.ac.jp>
2016-05-06 12:27:06 +0900

    プロジェクトを作った

---
+++ Altseed_Invader/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace Game
+{
+	class Program
+	{
+		[System.STAThread]
+		static void Main(string[] args)
+		{
+			// Altseedを初期化する。
+			asd.Engine.Initialize("Invader", 640, 480, new asd.EngineOption());
+			
+			// Altseedのウインドウが閉じられていないか確認する。
+			while (asd.Engine.DoEvents())
+			{
+				// Altseedを更新する。
+				asd.Engine.Update();
+			}
+
+			// Altseedの終了処理をする。
+			asd.Engine.Terminate();
+		}
+	}
+}
