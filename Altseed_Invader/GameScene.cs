b15635c3ad7ae5d374261903266255c3c3657588
wisteria <wisteria@coins.tsukuba.ac.jp>
2016-05-07 18:24:26 +0900

    レイヤー使うのやめた

--- Altseed_Invader/GameScene.cs
+++ Altseed_Invader/GameScene.cs
@@ -10,7 +10,21 @@ namespace Game
 	{
 		public GameScene()
 		{
-			AddLayer(new ObjectLayer());
+			asd.Layer2D layer = new asd.Layer2D();
+
+			ControlableObject player = new ControlableObject();
+			layer.AddObject(player);
+
+			Random random = new Random();
+			for (int x = 0; x < 10; x++)
+			{
+				for (int y = 0; y < 6; y++)
+				{
+					FloatingObject enemy = new FloatingObject(new asd.Vector2DF(95 + x * 50.0f, 50 + y * 50.0f), y % 3, random);
+					layer.AddObject(enemy);
+				}
+			}
+
 		}
 	}
 }
